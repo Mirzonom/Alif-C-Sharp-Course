e038f7dc0a67efea423a4fcdf7143e5ae3467c8c
Inom Dev <77639035+Mirzonom@users.noreply.github.com>
2024-01-25 15:34:18 +0500

    PlayerBehavior.cs

---
+++ practice/24/24.3/PlayerBehavior.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Mario
+{
+    public class PlayerBehavior : IPlayerBehavior
+    {
+        public PlayerState State { get; set; }
+
+        public void Attack()
+        {
+            // ...
+        }
+
+        public void Move()
+        {
+            // ...
+        }
+
+        public void PickUpCoin()
+        {
+            // ...
+        }
+    }
+}
