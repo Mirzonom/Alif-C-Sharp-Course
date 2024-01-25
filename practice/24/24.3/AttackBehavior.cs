afb191c79b7a67d06691b157b85dd246bedf346d
Inom Dev <77639035+Mirzonom@users.noreply.github.com>
2024-01-25 15:35:13 +0500

    AttackBehavior.cs

---
+++ practice/24/24.3/AttackBehavior.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Mario
+{
+    public class AttackBehavior : IPlayerBehavior
+    {
+        public PlayerState State { get; set; }
+
+        public void Attack()
+        {
+            // ...
+        }
+
+        public void Move()
+        {
+            // ...
+        }
+
+        public void PickUpCoin()
+        {
+            // ...
+        }
+    }
+}
