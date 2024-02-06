d58beab789676d3e8984c2854e03e6b7b7797bf6
Inom Dev <77639035+Mirzonom@users.noreply.github.com>
2024-02-06 11:29:51 +0500

    Fruit.cs

---
+++ practice/26/Fruit.cs
@@ -0,0 +1,16 @@
+using System; 
+
+public class Fruit
+{
+    public string Name { get; set; }
+}
+
+public class Apple : Fruit
+{
+    public bool IsRed { get; set; }
+}
+
+public class Banana : Fruit
+{
+    public bool IsRipe { get; set; }
+}
